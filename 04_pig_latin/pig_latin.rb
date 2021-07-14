a6465425d6a5229bd35b52d7e3dacb7f4b73b6db
Adrian Salcudean <adriansalcudean@pop-os.localdomain>
2021-07-14 16:01:09 +0300

    Translate a single word

---
+++ 04_pig_latin/pig_latin.rb
@@ -0,0 +1,34 @@
+$vow = ['a', 'e', 'i', 'o', 'u']
+$con = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'w', 'v', 'x', 'y', 'z']
+
+def translate(str)
+  str.split.map { |curr_word|  translate_word(curr_word) }.join
+end
+
+def translate_word(word)
+  final_word = ''
+  word[0] = word[0].downcase
+
+  if $vow.include? word[0]
+    final_word += translate_vow(word)
+  else
+    final_word = +translate_con(word)
+  end
+
+  final_word
+end
+
+def translate_vow(word)
+  word + 'ay'
+end
+
+def translate_con(word)
+  new_word = ''
+  i = 0
+  while $con.include? word[i]
+    i += 1
+  end
+  new_word = new_word + word[i, word.length - 1] + word[0, i ] + 'ay'
+
+  new_word
+end
