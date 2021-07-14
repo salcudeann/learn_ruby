60c907b05df7d900a2a31c8fa71e3ccc96df8855
Adrian Salcudean <adriansalcudean@pop-os.localdomain>
2021-07-14 13:08:47 +0300

    Simplify ftoc function, remove unnecesarry comments

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,20 +1,11 @@
 def ftoc(val)
-  val = val-32
-  5.0*val/9.0
-
-=begin
-  if val==32
-    0
-  else
-    5.0*(val-32)/9.0
-  end
-=end
+  5.0 * (val - 32) / 9.0
 end
 
 def ctof(val)
   if val == 0
     32
   else
-    9.0*val/5.0 + 32
+    9.0 * val / 5.0 + 32
   end
 end
