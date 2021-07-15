6b724eb7ee6be5a1ef39f32310ea24a48408d1f5
Adrian Salcudean <adriansalcudean@pop-os.localdomain>
2021-07-15 15:10:17 +0300

    Remove global variables

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,33 +1,28 @@
-$vow = ['a', 'e', 'i', 'o', 'u']
-$con = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'w', 'v', 'x', 'y', 'z']
-
 def translate(str)
-  str.split.map { |curr_word|  translate_word(curr_word) }.join(' ')
+  str.split.map { |curr_word| translate_word(curr_word) }.join(' ')
 end
 
 def translate_word(word)
+  vowels = ['a', 'e', 'i', 'o', 'u']
+  con = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'w', 'v', 'x', 'y', 'z']
   final_word = ''
   word[0] = word[0].downcase
 
-  if $vow.include? word[0]
-    final_word += translate_vow(word)
+  if vowels.include? word[0]
+    final_word += word + 'ay'
   else
-    final_word = +translate_con(word)
-  end
-end
-
-def translate_vow(word)
-  word + 'ay'
-end
-
-def translate_con(word)
-  new_word = ''
-  i = 0
-  while $con.include? word[i]
-    if (word[i]=='q') && word[i+1]=='u'
-      i = i+1
+    new_word = ''
+    i = 0
+    while con.include? word[i]
+      if (word[i] == 'q') && word[i + 1] == 'u'
+        i = i + 1
+      end
+      i += 1
     end
-    i += 1
+    new_word = new_word + word[i, word.length - 1] + word[0, i] + 'ay'
+    final_word = final_word + new_word
+
   end
-  new_word = new_word + word[i, word.length - 1] + word[0, i ] + 'ay'
+  final_word
 end
+
