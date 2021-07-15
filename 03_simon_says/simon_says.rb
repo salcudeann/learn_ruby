b4232ba8de4f797360c7153b0690d03beecaadce
Adrian Salcudean <adriansalcudean@pop-os.localdomain>
2021-07-15 14:26:58 +0300

    Optimize titleize method

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -21,24 +21,11 @@ end
 
 def titleize(str)
   little_words = ['and', 'over', 'the']
-  ret_str = ''
-  str = str + ' '
-  words = str.split(' ')
 
-  if (words.length == 1)
-    ret_str = words[0]
-    ret_str[0] = ret_str[0].upcase 
-    ret_str
-  else
-    for i in 0..words.length - 1
-      if (!little_words.include? words[i]) || ((i == 0) && (little_words.include? words[i]))
-        words[i][0] = words[i][0].upcase
-      end
-      ret_str = ret_str + words[i] + ' '
-      puts ret_str
-    end
+  ret_str = str.split.map { |word| little_words.include?(word) ? word : word.capitalize }.join(" ")
 
-    ret_str[0, ret_str.length - 1]
+  if little_words.each { |word| ret_str[0] = ret_str[0].upcase if ret_str[0] == word[0] }
   end
 
+  ret_str
 end
