7cb2666bec6d7d903033860f542a445d7456e12f
Adrian Salcudean <adriansalcudean@pop-os.localdomain>
2021-07-15 15:31:32 +0300

    Remove final_word variable

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -5,13 +5,11 @@ end
 def translate_word(word)
   vowels = ['a', 'e', 'i', 'o', 'u']
   con = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'w', 'v', 'x', 'y', 'z']
-  final_word = ''
   word[0] = word[0].downcase
 
-  if vowels.include? word[0]
-    final_word += word + 'ay'
+  if word[0].start_with?(*vowels)
+     word + 'ay'
   else
-    new_word = ''
     i = 0
     while con.include? word[i]
       if (word[i] == 'q') && word[i + 1] == 'u'
@@ -19,10 +17,7 @@ def translate_word(word)
       end
       i += 1
     end
-    new_word = new_word + word[i, word.length - 1] + word[0, i] + 'ay'
-    final_word = final_word + new_word
-
+    word[i, word.length - 1] + word[0, i] + 'ay'
   end
-  final_word
 end
 
