f58c6451d87bcc1fcdf9ae2cd810e8a817053956
Adrian Salcudean <adriansalcudean@pop-os.localdomain>
2021-07-19 15:11:59 +0300

    Resolve dictionary test

---
+++ 11_dictionary/dictionary.rb
@@ -0,0 +1,52 @@
+class Dictionary
+  attr_reader :hash
+
+  def initialize
+    @hash = {}
+  end
+
+  def entries
+    @hash
+  end
+
+  def add(key)
+    if key.is_a? String
+      @hash[key] = nil
+    else
+      key.each do |k, v|
+        @hash[k] = v
+      end
+    end
+  end
+
+  def keywords()
+    key_array = []
+    @hash.each { |k, v| key_array.push(k) }
+    key_array.sort
+  end
+
+  def include?(key)
+    @hash.has_key?(key) ? true : false
+  end
+
+  def find(str)
+    found_hash = {}
+    if str.is_a? String
+      @hash.each do |k, v|
+        if k[0...str.length] == str
+          found_hash[k] = v
+        end
+      end
+      found_hash
+    end
+  end
+
+  def printable
+    str = ""
+    @hash.reverse_each.each do |key, value|
+      str = str + "[" + key + "]" + " " + "\"" + value + "\"\n"
+    end
+    str = str[0, str.length - 1]
+  end
+
+end
